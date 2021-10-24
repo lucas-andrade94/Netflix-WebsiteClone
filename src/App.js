d2382dee5de24b936b2e6692d2c1f77d47b4dff8
Lucas Andrade <andradelucas94@gmail.com>
2021-10-23 18:22:02 -0600

    Fixing eslint warnings

--- src/App.js
+++ src/App.js
@@ -1,63 +1,63 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
-import Api from "./Api";
-import MovieRow from "./components/MovieRow";
-import FeaturedMovie from "./components/FeaturedMovie";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import { getHomeList, getMovieInfo } from "./Api";
+import { MovieRow } from "./components/MovieRow";
+import { FeaturedMovie } from "./components/FeaturedMovie";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
-    const [movieList, setMovieList] = useState([]);
-    const [featuredData, setFeaturedData] = useState(null);
-    const [blackHeader, setBlackHeader] = useState(false);
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
-    useEffect(() => {
-        const loadAll = async () => {
-            //Picking up the whole list
-            let list = await Api.getHomeList();
-            setMovieList(list);
+  useEffect(() => {
+    const loadAll = async () => {
+      //Picking up the whole list
+      let list = await getHomeList();
+      setMovieList(list);
 
-            //Picking up the featured movie
-            let originals = list.filter((i) => i.slug === "originals");
-            let randomChosen = Math.floor(
-                Math.random() * (originals[0].items.results.length - 1)
-            );
-            let chosen = originals[0].items.results[randomChosen];
-            let chosenInfo = await Api.getMovieInfo(chosen.id, "tv");
-            setFeaturedData(chosenInfo);
-        };
+      //Picking up the featured movie
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await getMovieInfo(chosen.id, "tv");
+      setFeaturedData(chosenInfo);
+    };
 
-        loadAll();
-    }, []);
+    loadAll();
+  }, []);
 
-    //Header transitioning to black background when scroll down the page
-    useEffect(() => {
-        const scrollListener = () => {
-            if (window.scrollY > 300) {
-                setBlackHeader(true);
-            } else {
-                setBlackHeader(false);
-            }
-        };
+  //Header transitioning to black background when scroll down the page
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 300) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    };
 
-        window.addEventListener("scroll", scrollListener);
-        return () => {
-            window.removeEventListener("scroll", scrollListener);
-        };
-    }, []);
+    window.addEventListener("scroll", scrollListener);
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
 
-    return (
-        <div className="page">
-            <Header black={blackHeader} />
-            {featuredData && <FeaturedMovie item={featuredData} />}
-            <section className="lists">
-                {movieList.map((item, key) => (
-                    <MovieRow key={key} title={item.title} items={item.items} />
-                ))}
-            </section>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="page">
+      <Header black={blackHeader} />
+      {featuredData && <FeaturedMovie item={featuredData} />}
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,23 @@
 import React from "react";
 import "./Header.css";
 
-export default ({ black }) => {
-    return (
-        <header className={black ? "black" : ""}>
-            <div className="header--logo">
-                <a href="/">
-                    <img
-                        src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo.png"
-                        alt="Netflix Logo"
-                    />
-                </a>
-            </div>
-            <div className="header--user">
-                <img
-                    src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png"
-                    alt="Profile"
-                />
-            </div>
-        </header>
-    );
-};
+export function Header({ black }) {
+  return (
+    <header className={black ? "black" : ""}>
+      <div className="header--logo">
+        <a href="/">
+          <img
+            src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo.png"
+            alt="Netflix Logo"
+          />
+        </a>
+      </div>
+      <div className="header--user">
+        <img
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png"
+          alt="Profile"
+        />
+      </div>
+    </header>
+  );
+}
